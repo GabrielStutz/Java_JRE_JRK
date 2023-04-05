a616db2980bb010b3ac0e558938215bcb0ddcd24
Gabriel Vieira Stutz <gabrielstutz100@gmail.com>
2023-04-05 00:05:55 -0300

    Beggining of Java(variables).

---
+++ src/TestaConversao.java
@@ -0,0 +1,13 @@
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		float pontoFlutuante = 3.14f;
+		int valor = (int) salario;
+		System.out.println(valor);
+	
+		long numeroGrande = 3232323232323232L;
+		short valorPequeno = 2123;
+		byte b = 127;
+	}
+}
